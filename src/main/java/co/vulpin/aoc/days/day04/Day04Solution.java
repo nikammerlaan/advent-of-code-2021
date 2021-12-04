a9bf0803131023932ee6b728e01e1d1e364ec8f8
Nik <nikammerlaan@gmail.com>
2021-12-03 22:18:01 -0800

    Use new solution

--- src/main/java/co/vulpin/aoc/days/day04/Day04Solution.java
+++ src/main/java/co/vulpin/aoc/days/day04/Day04Solution.java
@@ -3,23 +3,21 @@ package co.vulpin.aoc.days.day04;
 import co.vulpin.aoc.days.DaySolution;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Day04Solution extends DaySolution<Day04Solution.Input> {
+public class Day04Solution extends DaySolution<String> {
 
     @Override
-    protected Object solvePart1(Input input) {
-        var calledNums = new HashSet<Integer>();
+    protected Object solvePart1(String rawInput) {
+        var input = parseInput0(rawInput);
 
-        for(int num : input.numbers()) {
-            calledNums.add(num);
+        for(var num : input.numbers()) {
+            num.mark();
 
             for(var board : input.boards()) {
-                if(hasWon(board, calledNums)) {
-                    return num * Arrays.stream(board)
-                        .flatMapToInt(Arrays::stream)
-                        .filter(i -> !calledNums.contains(i))
-                        .sum();
+                if(board.hasWon()) {
+                    return board.getWinningValue(num.value());
                 }
             }
         }
@@ -28,26 +26,25 @@ public class Day04Solution extends DaySolution<Day04Solution.Input> {
     }
 
     @Override
-    protected Object solvePart2(Input input) {
-        var calledNums = new HashSet<Integer>();
+    protected Object solvePart2(String rawInput) {
+        var input = parseInput0(rawInput);
+
         var boards = new ArrayList<>(input.boards());
 
-        for(int num : input.numbers()) {
-            calledNums.add(num);
+        for(var num : input.numbers()) {
+            System.out.println(num.value());
+            num.mark();
 
-            var newBoards = new ArrayList<int[][]>();
+            var newBoards = new ArrayList<Board>();
 
             for(var board : boards) {
-                if(!hasWon(board, calledNums)) {
+                if(!board.hasWon()) {
                     newBoards.add(board);
                 }
             }
 
             if(newBoards.isEmpty()) {
-                return num * Arrays.stream(boards.get(0))
-                    .flatMapToInt(Arrays::stream)
-                    .filter(i -> !calledNums.contains(i))
-                    .sum();
+                return boards.get(0).getWinningValue(num.value());
             }
 
             boards = newBoards;
@@ -56,47 +53,25 @@ public class Day04Solution extends DaySolution<Day04Solution.Input> {
         throw new IllegalStateException();
     }
 
-    private boolean hasWon(int[][] board, Set<Integer> nums) {
-        for(int i = 0; i < 5; i++) {
-            if(hasWonRow(board, i, nums)) {
-                return true;
-            }
-        }
-
-        for(int i = 0; i < 5; i++) {
-            if(hasWonColumn(board, i, nums)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private boolean hasWonRow(int[][] board, int row, Set<Integer> nums) {
-        for(int i = 0; i < 5; i++) {
-            if(!nums.contains(board[row][i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean hasWonColumn(int[][] board, int column, Set<Integer> nums) {
-        for(int i = 0; i < 5; i++) {
-            if(!nums.contains(board[i][column])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     @Override
-    protected Input parseInput(String input) {
+    protected String parseInput(String input) {
+        return input;
+    }
+
+    protected Input parseInput0(String input) {
         var lines = input.lines()
             .collect(Collectors.toCollection(LinkedList::new));
 
-        var numbers = Arrays.stream(lines.pop().split(","))
+        var rawCalledNumbers = Arrays.stream(lines.pop().split(","))
             .map(Integer::parseInt)
+            .toList();
+
+        var numbers = rawCalledNumbers.stream()
+            .map(Number::new)
+            .collect(Collectors.toMap(Number::value, Function.identity()));
+
+        var calledNumbers = rawCalledNumbers.stream()
+            .map(numbers::get)
             .toList();
 
         var boards = Arrays.stream(String.join("\n", lines).split("\n\n"))
@@ -104,16 +79,87 @@ public class Day04Solution extends DaySolution<Day04Solution.Input> {
             .map(rawBoard -> Arrays.stream(rawBoard.split("\n"))
                 .map(s -> Arrays.stream(s.split("\\s+"))
                     .filter(s0 -> !s0.isBlank())
-                    .mapToInt(Integer::parseInt)
-                    .toArray()
+                    .map(Integer::parseInt)
+                    .map(numbers::get)
+                    .toArray(Number[]::new)
                 )
-                .toArray(int[][]::new)
+                .toArray(Number[][]::new)
             )
+            .map(Board::new)
             .toList();
 
-        return new Input(numbers, boards);
+        return new Input(calledNumbers, boards);
     }
 
-    record Input(List<Integer> numbers, List<int[][]> boards) {}
+    record Board(Number[][] numbers) {
+
+        public int getWinningValue(int number) {
+            return number * Arrays.stream(numbers)
+                .flatMap(Arrays::stream)
+                .filter(i -> !i.marked())
+                .mapToInt(Number::value)
+                .sum();
+        }
+
+        public boolean hasWon() {
+            for(int i = 0; i < 5; i++) {
+                if(hasWonRow(i)) {
+                    return true;
+                }
+            }
+
+            for(int i = 0; i < 5; i++) {
+                if(hasWonColumn(i)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private boolean hasWonRow(int row) {
+            for(int i = 0; i < 5; i++) {
+                if(!numbers[row][i].marked()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private boolean hasWonColumn(int column) {
+            for(int i = 0; i < 5; i++) {
+                if(!numbers[i][column].marked()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+
+    private static class Number {
+
+        private final int value;
+        private boolean marked;
+
+        public Number(int value) {
+            this.value = value;
+            this.marked = false;
+        }
+
+        public int value() {
+            return value;
+        }
+
+        public boolean marked() {
+            return marked;
+        }
+
+        public void mark() {
+            this.marked = true;
+        }
+    }
+
+    record Input(List<Number> numbers, List<Board> boards) {}
 
 }
