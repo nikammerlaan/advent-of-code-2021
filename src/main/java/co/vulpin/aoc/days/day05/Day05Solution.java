16f4ac3849d488b433fe464da55f53bf433415d7
Nik <nikammerlaan@gmail.com>
2021-12-05 14:20:09 -0800

    Refactor Day05Solution to be more efficient and use same code for both

--- src/main/java/co/vulpin/aoc/days/day05/Day05Solution.java
+++ src/main/java/co/vulpin/aoc/days/day05/Day05Solution.java
@@ -1,39 +1,37 @@
 package co.vulpin.aoc.days.day05;
 
-import co.vulpin.aoc.days.AbstractSeparateDaySolution;
+import co.vulpin.aoc.data.SolveResult;
+import co.vulpin.aoc.data.TimeResult;
+import co.vulpin.aoc.days.AbstractJoinedDaySolution;
 import co.vulpin.aoc.misc.Point;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Day05Solution extends AbstractSeparateDaySolution<List<Day05Solution.Input>> {
+public class Day05Solution extends AbstractJoinedDaySolution<List<Day05Solution.Line>> {
 
     @Override
-    protected Object solvePart1(List<Input> input) {
-        return solve(input, false);
-    }
+    protected SolveResult solve(List<Line> input) {
+        var start = Instant.now();
 
-    @Override
-    protected Object solvePart2(List<Input> input) {
-        return solve(input, true);
-    }
-
-    private long solve(List<Input> input, boolean diagonals) {
-        int xLength = 1 + input.stream()
+        var xLength = 1 + input.stream()
             .flatMap(i -> Stream.of(i.from(), i.to()))
             .mapToInt(Point::x)
             .max()
             .orElseThrow();
-        int yLength = 1 + input.stream()
+        var yLength = 1 + input.stream()
             .flatMap(i -> Stream.of(i.from(), i.to()))
             .mapToInt(Point::y)
             .max()
             .orElseThrow();
 
-        int[][] board = new int[xLength][yLength];
+        var board = new int[xLength][yLength];
 
-
+        var diagonals = new LinkedList<Line>();
         for(var line : input) {
             var from = line.from();
             var to = line.to();
@@ -46,49 +44,62 @@ public class Day05Solution extends AbstractSeparateDaySolution<List<Day05Solutio
                 for(int i = Math.min(from.x(), to.x()); i <= Math.max(from.x(), to.x()); i++) {
                     board[i][from.y()]++;
                 }
-            } else if(diagonals){
-                boolean xAscending = from.x() < to.x();
-                boolean yAscending = from.y() < to.y();
-
-                int x = from.x();
-                int y = from.y();
-
-                while(x != to.x() && y != to.y()) {
-                    board[x][y]++;
-
-                    if(xAscending) {
-                        x++;
-                    } else {
-                        x--;
-                    }
-                    if(yAscending) {
-                        y++;
-                    } else {
-                        y--;
-                    }
-                }
-
-                board[x][y]++;
+            } else {
+                diagonals.add(line);
             }
         }
 
+        var part1 = new TimeResult<>(getOverlapCount(board), Duration.between(start, Instant.now()));
+
+        for(var line : diagonals) {
+            var from = line.from();
+            var to = line.to();
+
+            var xAscending = from.x() < to.x();
+            var yAscending = from.y() < to.y();
+
+            var x = from.x();
+            var y = from.y();
+
+            while(x != to.x() && y != to.y()) {
+                board[x][y]++;
+
+                if(xAscending) {
+                    x++;
+                } else {
+                    x--;
+                }
+                if(yAscending) {
+                    y++;
+                } else {
+                    y--;
+                }
+            }
+            board[x][y]++;
+        }
+
+        var part2 = new TimeResult<>(getOverlapCount(board), Duration.between(start, Instant.now()));
+
+        return new SolveResult(part1, part2);
+    }
+
+    private long getOverlapCount(int[][] board) {
         return Arrays.stream(board)
             .flatMapToInt(Arrays::stream)
             .filter(i -> i >= 2)
             .count();
     }
 
-
     @Override
-    protected List<Input> parseInput(String input) {
+    protected List<Line> parseInput(String input) {
         return Arrays.stream(input.split("\n"))
             .map(this::parseInputLine)
             .toList();
     }
 
-    private Input parseInputLine(String input) {
+    private Line parseInputLine(String input) {
         var parts = input.split(" -> ");
-        return new Input(parseInputPoint(parts[0]), parseInputPoint(parts[1]));
+        return new Line(parseInputPoint(parts[0]), parseInputPoint(parts[1]));
     }
 
     private Point parseInputPoint(String input) {
@@ -96,6 +107,6 @@ public class Day05Solution extends AbstractSeparateDaySolution<List<Day05Solutio
         return new Point(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
     }
 
-    record Input(Point from, Point to) {}
+    record Line(Point from, Point to) {}
 
 }
