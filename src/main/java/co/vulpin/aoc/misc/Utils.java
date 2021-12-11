24eafc04bc6cbec4ff7f5e7db3ebccae3cdc93d5
Nik <nikammerlaan@gmail.com>
2021-12-11 14:05:50 -0800

    Add Timer class for easier timing of durations

--- src/main/java/co/vulpin/aoc/misc/Utils.java
+++ src/main/java/co/vulpin/aoc/misc/Utils.java
@@ -2,17 +2,16 @@ package co.vulpin.aoc.misc;
 
 import co.vulpin.aoc.data.TimeResult;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.function.Supplier;
 
 public class Utils {
 
     public static <T> TimeResult<T> timeExecution(Supplier<T> supplier) {
-        var start = Instant.now();
+        var timer = new Timer();
+        timer.start();
         var t = supplier.get();
-        var end = Instant.now();
-        return new TimeResult<>(t, Duration.between(start, end));
+        var duration = timer.stop();
+        return new TimeResult<>(t, duration);
     }
 
 }
